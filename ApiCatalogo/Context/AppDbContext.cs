2ead494e625fda65df3b8f31ffc4094f9f0ca30e
Thacio Helmer <thacio.helmer@el.com.br>
2022-09-09 11:00:24 -0300

    Minimal API EFCore Net6

---
+++ ApiCatalogo/Context/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using ApiCatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiCatalogo.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Produto>? Produtos { get; set; }
+        public DbSet<Categoria>? Categorias { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Categoria>().HasKey(c => c.CategoriaId);
+            modelBuilder.Entity<Categoria>().Property(c => c.Nome).HasMaxLength(100).IsRequired();
+            modelBuilder.Entity<Categoria>().Property(c => c.Descricao).HasMaxLength(150).IsRequired();
+
+            modelBuilder.Entity<Produto>().HasKey(p => p.ProdutoId);
+            modelBuilder.Entity<Produto>().Property(p => p.Nome).HasMaxLength(100).IsRequired();
+            modelBuilder.Entity<Produto>().Property(p => p.Descricao).HasMaxLength(150).IsRequired();
+            modelBuilder.Entity<Produto>().Property(p => p.Imagem).HasMaxLength(150).IsRequired();
+            modelBuilder.Entity<Produto>().Property(p => p.Preco).HasPrecision(14, 2);
+
+            modelBuilder.Entity<Produto>().HasOne<Categoria>(c => c.Categoria).WithMany(p => p.Produtos).HasForeignKey(c => c.CategoriaId);
+        }
+    }
+}
